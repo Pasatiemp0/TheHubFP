68cd256be81c17eed9bf76bf50cc85aff4260a0b
Pasatiemp0 <slumber.intakes0j@icloud.com>
2025-11-18 15:31:30 +0100

    Add 'adivina_java'

---
+++ Conversor_de_divisas.java
@@ -0,0 +1,8 @@
+import 
+
+public class Conversor_de_divisas {
+    final double USD = 1.1;
+    final double GBP = 0.8;
+    final double JPY = 179.8;
+    CurrencyUnit usd = Monetary.getCurrency("USD");
+}
