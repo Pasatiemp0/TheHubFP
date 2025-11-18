ba4e7bccd22cacea16b78fd1af00a562d71ab07b
Pasatiemp0 <slumber.intakes0j@icloud.com>
2025-11-18 15:35:41 +0100

    modified:   Conversor_de_divisas.java 	modified:   adivina_numero.java

--- Conversor_de_divisas.java
+++ Conversor_de_divisas.java
@@ -1,8 +1,12 @@
-import 
+
 
 public class Conversor_de_divisas {
     final double USD = 1.1;
     final double GBP = 0.8;
     final double JPY = 179.8;
-    CurrencyUnit usd = Monetary.getCurrency("USD");
+
+    public static void main(String[] args) {
+        System.out.println("Monedas admitidas para la conversión: ");
+        System.out.println("");
+    }
 }
---
+++ adivina_numero.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class adivina_numero {
+
+    public static void main(String[] args) {
+        int numeroAleatorio = (int) Math.random()*100 +1;
+    }
+    
+}
