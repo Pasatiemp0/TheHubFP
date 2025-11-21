1646fc0744352f9c5d0738e80c82179d3e00b159
Pasatiemp0 <105547039+Pasatiemp0@users.noreply.github.com>
2025-11-21 08:26:46 +0000

    modified:   reto2EDCorregir.java

--- reto2EDCorregir.java
+++ reto2EDCorregir.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
-public class reto2ED {
+public class reto2EDCorregir {
     static Scanner teclado = new Scanner(System.in);
 
     public static int m (){
         int opcion = 1;
         boolean esperandoOpcion = true;
         System.out.println("<<< Operacion a realizar >>>");
-        System.out.print("1 - Sumar");System.out.println("2 - Restar")
-        System.out.println("3 - Multiplicar");System.out.println("4 - Dividir");
+        System.out.print("1 - Sumar");
+        System.out.println("2 - Restar");
+        System.out.println("3 - Multiplicar");
+        System.out.println("4 - Dividir");
         System.out.println("0 - Finalizar");
         int eleccion = 0;
 
         do {
             System.out.println("Introduce un valor entre 0 y 4:");
-            System.out.print("> ")eleccion = teclado.nextInt();
+            System.out.print("> ");
+            eleccion = teclado.nextInt();
 
             if ( (opcion <= 0) && (opcion >= 4) ) {
                 esperandoOpcion = false;
@@ -24,17 +27,17 @@ public class reto2ED {
     }
 
     public static int[] n(){
-            int n1, n2;
+        int n1, n2;
     int[] valores = new int [2];
-                        System.out.print("Introduce numero:");
+        System.out.print("Introduce numero:");
         n1 = teclado.nextDouble() System.out.print("Introduce numero:");
 n2 = teclado.nextInt();
     return new int[]{n1, n2};
     }
 
     public static int s(int n1, int n2) { return n1-n2}; 
-    publicstatic int r (int n1, int n2) { return n1-n2;};
-    publicstaticintm(int n1, int n2) { return n1*n2}; 
+    public static int r (int n1, int n2) { return n1-n2;};
+    public static int m(int n1, int n2) { return n1*n2}; 
     public int d (int n1, int n2) { return n1/n2;};
 
 
