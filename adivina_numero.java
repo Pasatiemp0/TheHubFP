374a8d2192825a320fa65d3e009a4bb90b08b86b
Pasatiemp0 <slumber.intakes0j@icloud.com>
2025-11-21 09:21:10 +0100

    new file:   reto2EDCorregir.java

--- adivina_numero.java
+++ adivina_numero.java
@@ -4,6 +4,16 @@ public class adivina_numero {
 
     public static void main(String[] args) {
         int numeroAleatorio = (int) Math.random()*100 +1;
-    }
+        boolean adivinado = false;
+        do {
+            if (input == numeroAleatorio){
+
+            }else if (input < numeroAleatorio) {
+
+            }else{
+                
+            }
+    
+        } while (!adivinado)
     
 }
