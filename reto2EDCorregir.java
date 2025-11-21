a49f470af2ae3b9318fe475532f76af8c660bfb1
Pasatiemp0 <105547039+Pasatiemp0@users.noreply.github.com>
2025-11-21 10:05:53 +0000

    modified:   reto2EDCorregir.java

--- reto2EDCorregir.java
+++ reto2EDCorregir.java
@@ -1,70 +1,104 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class reto2EDCorregir {
     static Scanner teclado = new Scanner(System.in);
 
-    public static int m (){
-        int opcion = 1;
+    public static int menu() {
+        int opcion = -1;
         boolean esperandoOpcion = true;
-        System.out.println("<<< Operacion a realizar >>>");
-        System.out.print("1 - Sumar");
+        System.out.println("<<< Menú Operacion >>>");
+        System.out.println("1 - Sumar");
         System.out.println("2 - Restar");
         System.out.println("3 - Multiplicar");
         System.out.println("4 - Dividir");
         System.out.println("0 - Finalizar");
-        int eleccion = 0;
 
         do {
-            System.out.println("Introduce un valor entre 0 y 4:");
-            System.out.print("> ");
-            eleccion = teclado.nextInt();
+            try {
+                System.out.println("Introduce un valor entre 0 y 4");
+                System.out.print("> ");
+                opcion = teclado.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Debes introducir un numero valido correspondiente al Menú.");
+                teclado.next();
+                continue;
+            }
 
-            if ( (opcion <= 0) && (opcion >= 4) ) {
+            if ((opcion <= 4) && (opcion >= 0)) {
                 esperandoOpcion = false;
             }
-        } while(esperandoOpcion);
+        } while (esperandoOpcion);
         return opcion;
     }
 
-    public static int[] n(){
+    public static int[] numeroInput(int seleccion) {
         int n1, n2;
-    int[] valores = new int [2];
-        System.out.print("Introduce numero:");
-        n1 = teclado.nextDouble() System.out.print("Introduce numero:");
-n2 = teclado.nextInt();
-    return new int[]{n1, n2};
+        System.out.println("Introduce el primer numero de la operacion");
+        System.out.print("> ");
+        n1 = teclado.nextInt();
+        do {
+            System.out.println("Introduce el segundo numero de la operacion");
+            System.out.print("> ");
+            n2 = teclado.nextInt();
+            if (seleccion == 4 && n2 == 0) {
+                System.out.println("No se puede dividir entre cero.");
+            }
+        } while (seleccion == 4 && n2 == 0);
+
+        int[] valores = new int[] { n1, n2 };
+        return valores;
     }
 
-    public static int s(int n1, int n2) { return n1-n2}; 
-    public static int r (int n1, int n2) { return n1-n2;};
-    public static int m(int n1, int n2) { return n1*n2}; 
-    public int d (int n1, int n2) { return n1/n2;};
+    public static int sumar(int n1, int n2) {
+        return n1 + n2;
+    }
 
+    public static int restar(int n1, int n2) {
+        return n1 - n2;
+    }
 
-    public static void main(String[] args) {int m=m();
-  int[]d=n();
-                String texto=null;                    int ro = 0
-        
-        switch (m){
-            case 1:
-ro = s(d[0], d[1]);
-            System.out.println("Los resultados de la Suma son " + ro);
-            break;case 2:
-            ro = m(d[0], d[1]);System.out.println("Los resultados de la Resta son " + ro);break;case 3:ro = m(datos[0], datos[1]);
-                System.out.println("Los resultados de la Multiplicacion son " + ro);break;
-            case 4:
-            ro = d(d[0], d[1]);
-            System.out.println("Los resultados de la Division son " + ro);
-            default:
-            System.out.println("TExto sin más, un simple error: " + texto.length());
-            System.out.println("Bye-bye");}
-        d[3] = ro;
-        System.out.println("Se han realizado las operaciones y el reslutado es " + ro);
-        
-        while (ro > 0){System.out.println("La mitad del resultado es " + ro/2);if ( (ro/2) >= 0) {System.out.println("Aun se puede partit por la mitad");}
+    public static int multiplicar(int n1, int n2) {
+        return n1 * n2;
+    }
+
+    public static double dividir(int n1, int n2) {
+        return (double) n1 / n2;
+    }
+
+    public static void main(String[] args) {
+        int operador = menu();
+        if (operador == 0) {
+            System.out.println("Bye-bye");
+            System.exit(0);
         }
-        
+        int[] dosnumeros = numeroInput(operador);
+        double resultado = 0;
+
+        switch (operador) {
+            case 1:
+                resultado = sumar(dosnumeros[0], dosnumeros[1]);
+                System.out.println("Los resultados de la Suma son " + resultado);
+                break;
+            case 2:
+                resultado = restar(dosnumeros[0], dosnumeros[1]);
+                System.out.println("Los resultados de la Resta son " + resultado);
+                break;
+            case 3:
+                resultado = multiplicar(dosnumeros[0], dosnumeros[1]);
+                System.out.println("Los resultados de la Multiplicacion son " + resultado);
+                break;
+            case 4:
+                resultado = dividir(dosnumeros[0], dosnumeros[1]);
+                System.out.println("Los resultados de la Division son " + resultado);
+                break;
+        }
+
+        System.out.println("Se han realizado las operaciones y el reslutado es: " + resultado);
+
+        System.out.println("La mitad del resultado es " + resultado / 2);
+        resultado = resultado / 2;
+        System.out.println("Aun se puede partir por la mitad: " + resultado / 2);
 
     }
-
 }
