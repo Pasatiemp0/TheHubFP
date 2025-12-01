1e38960c2261e4962c7b1c97f5acea650a66263b
Pasatiemp0 <slumber.intakes0j@icloud.com>
2025-12-01 10:09:28 +0100

    sync to container

--- Conversor_de_divisas.java
+++ Conversor_de_divisas.java
@@ -1,12 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+//EUR como divisa base
+//ej  USD a GBP: 100 -> 72.72
+//100 USD 
+//100/0.8*1.1=137.5
 
 public class Conversor_de_divisas {
-    final double USD = 1.1;
-    final double GBP = 0.8;
-    final double JPY = 179.8;
+    static double USD = 0.8;
+    static double JPY = 179.8;
+    static double GBP = 1.1;
+    static double RMB = 8.2;
+
+    static Scanner tec = new Scanner(System.in);
+
+    public static double solicitudDivisa (int int_solicitud) {
+        double divisa;
+
+        switch (int_solicitud) {
+            case 1:
+                divisa = 1;
+                break;
+            case 2:
+                divisa = USD;
+                break;
+            case 3:
+                divisa = JPY;
+                break;
+            case 4:
+                divisa = GBP;
+                break;
+            case 5:
+                divisa = RMB;
+                break;
+            default:
+                break;
+        }        
+        return divisa;
+    }
 
     public static void main(String[] args) {
+        double inputUsuario;
+        double divisa;
+        ArrayList<String> registro =new ArrayList<String>();
+        System.out.println("taza actual");
+
+        
         System.out.println("Monedas admitidas para la conversión: ");
-        System.out.println("");
+        System.out.println("Euro, Yenes, Dólares, Libras.");
+        System.out.println("1. Euro, 2. Dólares, 3. Yenes, 4. Libras, 5. Ren Ming Bi");
+        divisa = solicitudDivisa(tec.nextInt());
+        System.out.println("Cantidad");
+        divisa = tec.nextInt()/tec.nextDouble();
+        System.out.println("Destinado");
+        divisa = divisa*solicitudDivisa(tec.nextInt());
+
+
+        registro.add(null);
+        
+               
+
+
     }
 }
