2b2c430e5e984065cee0a355a4be0f1c662b453b
Pasatiemp0 <105547039+Pasatiemp0@users.noreply.github.com>
2026-04-14 12:22:29 +0000

    Changes to be committed:

---
+++ LecturaEscritura/Escritura.java
@@ -0,0 +1,16 @@
+package LecturaEscritura;
+
+import java.io.*;
+
+
+public class Escritura {
+
+    public static void escribir() {
+        
+    }
+    
+    public static void main(String[] args) {
+        File archivo = new File("/workspaces/TheHubFP/LecturaEscritura/archivo.text");
+
+    }
+}
