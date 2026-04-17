3de51976e3a5d3bf839b177fba9a4a2f2537c8e1
Pasatiemp0 <slumber.intakes0j@icloud.com>
2026-04-17 13:19:34 +0200

    Changes to be committed:

--- SQL/CsvReader.java
+++ SQL/CsvReader.java
@@ -6,26 +6,39 @@ import java.util.ArrayList;
 public class CsvReader {
 
     public static void main(String[] args) {
-        String file = "/WORKSPACE/TheHubFP/SQL/Rotten Tomatoes Movies.csv";
+        String file = "Rotten Tomatoes Movies.csv"; //Ruta del archivo .csv
         String line= "";
         String split = ",";
+        String output = "output.sql";               //Se crea en la dirección base el archivo final 
 
+         String[] headers;
         ArrayList<String[]> data = new ArrayList<>();
         
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             if ((line = br.readLine()) != null) {
-                String [] headers = line.split(split);
+                headers = line.split(split);
                 System.out.println("\033[32mHeaders: " + String.join(", ", headers) + "\033[0m");
             } 
-            int i = 0;
+
             while ((line = br.readLine()) != null) {
                 String [] lineValues = line.split(",?=(?:[^\"]*\"[^\"]*\")*[^\"]*$", -1);
-                data.add(lineValues);
-                System.out.println(lineValues[i++]);            
+                data.add(lineValues);          
             }
         } catch (Exception e) {
             System.out.println("\033[31mSomething happened: " + e.getMessage() + "\033[0m");
             // TODO: handle exception
         }
+        
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(output))) {
+            String creatTableQuery = "INSERT INTO Rotten Tomatooes Movies (" + headers + ")\nVALUES\n";
+            bw.write(creatTableQuery);
+            for (String[] lines : data) {
+                bw.append("(" + lines + ")")
+            }
+        } catch (Exception e) {
+            System.out.println("\033[31mSomething happened: " + e.getMessage() + "\033[0m");
+            // TODO: handle exception
+        }
+        
     }
 }
